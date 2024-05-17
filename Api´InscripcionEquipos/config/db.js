de1e43ddd83cba34d3780643037463918340032e
[brayan23] <[bdelgadomunoz@gmail.com]>
2024-05-17 10:19:41 -0500

    db

--- Api´InscripcionEquipos/config/db.js
+++ Api´InscripcionEquipos/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectionDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/GoSport')
+        await mongoose.connect('mongodb+srv://GoSport:e4t5La0iPpalsVTT@atlascluster.aiyeayr.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster')
         console.log("connection established")
     } catch (error) {
         console.log(error.message)
